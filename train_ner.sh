408f7dc97fa6c66a9116f1ecdf9e2ce81ad266c4
Vimal <vimal@Vimals-MacBook-Pro-4.local>
2019-05-01 18:39:39 +0530

    Further changing file type to pipe and addind extension

--- train_ner.sh
+++ train_ner.sh
@@ -1,10 +1,11 @@
-file="training_data"
+
+file="/root/training_data.tsv"
 cat - > $file
 
 logpath="train_ner.log"
 
 echo "$$ Starting ..." >> $logpath
 
-python3 train_wc.py --train_file training_data --checkpoint ./checkpoint/ner_ --caseless --fine_tune --high_way --co_train --least_iters 100
+python3 train_wc.py --train_file /root/training_data.tsv --checkpoint ./checkpoint/ner_ --caseless --fine_tune --high_way --co_train --least_iters 100
 
 echo "$$ Done" >> $logpath
