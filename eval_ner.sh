85c0a192d99b21d0b47eb713c46d7ff709cca227
Vimal <vimal@Vimals-MacBook-Pro-4.local>
2019-06-12 16:49:28 +0530

    Bug fixes for running in cuda

--- eval_ner.sh
+++ eval_ner.sh
@@ -7,7 +7,7 @@ logpath="parse_ner.log"
 
 echo "Starting ..." >> $logpath
 
-python3 seq_wc.py --load_arg checkpoint/ner/ner_4_cwlm_lstm_crf.json --load_check_point checkpoint/ner_ner_4_cwlm_lstm_crf.model --gpu 0 --input_file $file --output_file output.txt
+python3 seq_wc.py --load_arg checkpoint/ner_cwlm_lstm_crf.json --load_check_point checkpoint/ner_cwlm_lstm_crf.model --gpu 0 --input_file $file --output_file output.txt
 
 echo "Done..." >> $logpath
 
