57e513c34090876ae4cadbe4da5cd82b6a3fc0a3
Vimal <vimal@Vimals-MacBook-Pro-4.local>
2019-06-14 18:14:19 +0530

    stdout redirected to logpath in train_ner_cuda

--- train_ner_cuda.sh
+++ train_ner_cuda.sh
@@ -7,6 +7,6 @@ logpath="train_ner.log"
 
 echo "$$ Starting ..." >> $logpath
 
-python3 train_wc.py --train_file training_data.tsv --checkpoint ./checkpoint/ner_ --caseless --fine_tune --high_way --co_train --least_iters 10 --gpu 0 --rand_embedding
+python3 train_wc.py --train_file training_data.tsv --checkpoint ./checkpoint/ner_ --caseless --fine_tune --high_way --co_train --least_iters 10 --gpu 0 --rand_embedding 2>>$logpath
 
 echo "$$ Done" >> $logpath
