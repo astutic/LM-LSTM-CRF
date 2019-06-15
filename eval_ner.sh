8cb2fe9160d9e7439d2df2861216e1a2892ba248
Vimal <vimal@Vimals-MacBook-Pro-4.local>
2019-06-15 18:55:23 +0530

    Reset training parameters. Modified eval_ner.sh to not print diagnostics to stdout

--- eval_ner.sh
+++ eval_ner.sh
@@ -8,7 +8,7 @@ logpath="parse_ner.log"
 
 echo "Starting ..." >> $logpath
 
-python3 seq_wc.py --decode_type label --load_arg checkpoint/ner_cwlm_lstm_crf.json --load_check_point checkpoint/ner_cwlm_lstm_crf.model --gpu 0 --input_file $file --output_file $outfile
+out=`python3 seq_wc.py --decode_type label --load_arg checkpoint/ner_cwlm_lstm_crf.json --load_check_point checkpoint/ner_cwlm_lstm_crf.model --gpu 0 --input_file $file --output_file $outfile >> $logpath 2>>&1`
 
 echo "Done..." >> $logpath
 
