db6b09b81dc1cdf97084c13e05ac4b553f4caff0
Vimal <vimal@Vimals-MacBook-Pro-4.local>
2019-06-17 13:31:44 +0530

    Adding logs to train_ner_cuda

--- train_ner_cuda.sh
+++ train_ner_cuda.sh
@@ -7,6 +7,6 @@ logpath="train_ner.log"
 
 echo "$$ Starting ..." >> $logpath
 
-python3 train_wc.py --train_file training_data.tsv --checkpoint ./checkpoint/ner_ --caseless --fine_tune --high_way --co_train --least_iters 100 --epoch 200 --gpu 0 --rand_embedding 2>>$logpath
+python3 train_wc.py --train_file training_data.tsv --checkpoint ./checkpoint/ner_ --caseless --fine_tune --high_way --co_train --least_iters 200 --epoch 400 --gpu 0 --rand_embedding >>$logpath 2>&1
 
 echo "$$ Done" >> $logpath
